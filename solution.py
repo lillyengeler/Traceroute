0636ad91906daa7fed42be369201de560ab80882
lillyengeler <lre250@nyu.edu>
2021-12-11 03:06:06 -0500

    edits

--- solution.py
+++ solution.py
@@ -159,7 +159,7 @@ def get_route(hostname):
                     bytes = struct.calcsize("d")
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     # Fill in start
-                    preRtt = 1000 * (timeSent - timeReceived)
+                    preRtt = 1000 * (startedSelect - timeSent)
                     print("pre rtt = ", preRtt)
                     midRtt = str(round(preRtt, 2))
                     print("mid rtt = ", midRtt)
